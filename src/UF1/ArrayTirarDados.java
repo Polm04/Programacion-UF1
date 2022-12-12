db30cd6754aea32664953e6ba223a32ab1c6b27a
Polm04 <pol.mr.2003@gmail.com>
2022-12-12 13:53:59 +0100

    Ejercicio ArrayCubiletes.java

--- src/UF1/ArrayTirarDados.java
+++ src/UF1/ArrayTirarDados.java
@@ -17,37 +17,42 @@ public class ArrayTirarDados {
      */
     public static void main(String[] args) {
         int[] dado = new int[6];
-        for (int i = 1; i < dado.length; i++) {
+        for (int i = 0; i < dado.length; i++) {
             dado[i] = 0;
         }
-        int lanzamiento=simularLanzamiento(dado);
-        int[] contadorCaras=contadorTiradas(dado, lanzamiento);
-        for (int i = 0; i <dado.length; i++) {
-            System.out.println("La cara "+(i+1)+ " a salido "+dado[i]);
+        
+        for (int i = 0; i < 10; i++) {
+            int lanzamiento = simularLanzamiento(dado);
+            int[] contadorCaras = contadorTiradas(dado, lanzamiento);
         }
-          
+        
+        for (int i = 0; i < dado.length; i++) {
+            System.out.println("La cara " + (i + 1) + " a salido " + dado[i]);
+        }
+
     }
 
     private static int simularLanzamiento(int[] dado) {
         Random rd = new Random();
-        int lanzamiento = rd.nextInt(6)+1;
+        int lanzamiento = rd.nextInt(6) + 1;
         System.out.println(lanzamiento);
         return lanzamiento;
     }
+
     public static int[] contadorTiradas(int[] dado, int lanzamiento) {
         if (lanzamiento == 1) {
-            dado[1-1]++;
+            dado[1 - 1]++;
         } else if (lanzamiento == 2) {
-            dado[2-1]++;
+            dado[2 - 1]++;
         } else if (lanzamiento == 3) {
-            dado[3-1]++;
+            dado[3 - 1]++;
         } else if (lanzamiento == 4) {
-            dado[4-1]++;
-        } else if (lanzamiento == 5) {          
-            dado[5-1]++;
+            dado[4 - 1]++;
+        } else if (lanzamiento == 5) {
+            dado[5 - 1]++;
         } else {
-            dado[6-1]++;
-        }       
+            dado[6 - 1]++;
+        }
         return dado;
     }
 }
