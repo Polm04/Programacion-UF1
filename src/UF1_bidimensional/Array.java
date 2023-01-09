21a9e7f82a3623dd0d3901eafa43caacbad3447a
Polm04 <pol.mr.2003@gmail.com>
2023-01-09 13:17:32 +0100

    Dos dimensiones array

--- src/UF1_bidimensional/Array.java
+++ src/UF1_bidimensional/Array.java
@@ -4,6 +4,7 @@
  */
 package UF1_bidimensional;
 
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -28,13 +29,46 @@ public class Array {
         //tercera fila
         mapa[2][0]=6;
         mapa[2][1]=16;
-        
+              
+        System.out.println("**************************");
         for (int i = 0; i <mapa.length; i++) {
             for (int j = 0; j <mapa[i].length; j++) {
                 System.out.print(mapa[i][j]+"-");
             }
             System.out.println("");
         }
+        System.out.println("**************************");
+        System.out.println("Valors a 4");  
+        System.out.println("**************************");
+        for (int i = 0; i <mapa.length; i++) {
+            for (int j = 0; j <mapa[i].length; j++) {
+                mapa[i][j]=4;
+            }
+        }
+        //mostarlo
+        for (int i = 0; i <mapa.length; i++) {
+            for (int j = 0; j <mapa[i].length; j++) {
+                System.out.print(mapa[i][j]+"-");
+            }
+            System.out.println("");
+        }
+        System.out.println("**************************");
+        System.out.println("Valors aleatorios entre 1 y 100");  
+        System.out.println("**************************");
+        Random rd = new Random();
+        for (int i = 0; i <mapa.length; i++) {
+            for (int j = 0; j <mapa[i].length; j++) {
+                mapa[i][j]=rd.nextInt(100)+1;
+            }
+        }
+        //mostarlo
+        for (int i = 0; i <mapa.length; i++) {
+            for (int j = 0; j <mapa[i].length; j++) {
+                System.out.print(mapa[i][j]+"-");
+            }
+            System.out.println("");
+        }
+        
         int x,y;
         Scanner sc = new Scanner(System.in);
         System.out.println("Fica la fila que vols veure: ");
@@ -43,5 +77,13 @@ public class Array {
         y=sc.nextInt();
         
         System.out.println("El valor de la coordenada introduida es:"+mapa[x][y]);
+        
+        //pedir valores usuario
+        for (int i = 0; i <mapa.length; i++) {
+            for (int j = 0; j <mapa[i].length; j++) {
+                System.out.println("Pon valor para "+i+"-"+j);
+                mapa[i][j]=sc.nextInt();
+            }
+        }
     }
 }
