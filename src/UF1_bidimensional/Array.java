a2128a8e71475ffbcc8ad5cbea2581377a031e34
Polm04 <pol.mr.2003@gmail.com>
2023-01-09 12:27:20 +0100

    Dos dimensiones array

--- src/UF1_bidimensional/Array.java
+++ src/UF1_bidimensional/Array.java
@@ -8,6 +8,23 @@ package UF1_bidimensional;
  *
  * @author pomo6989
  */
-public class Array {
-    
+public class Array {  
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        final int Files=3;
+        final int colum=2;
+        int[][] mapa = new int [Files][colum];
+        
+        //primera fila
+        mapa[0][0]=4;
+        mapa[0][1]=14;
+        //segunda fila
+        mapa[1][0]=5;
+        mapa[1][1]=15;
+        //tercera fila
+        mapa[2][0]=6;
+        mapa[2][1]=16;
+    }
 }
