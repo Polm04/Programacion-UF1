15814b973d737346edb028e095dbb1d1a8101b48
Polm04 <pol.mr.2003@gmail.com>
2023-02-06 14:07:53 +0100

    herencia persona:alumno i  profesor

---
+++ src/Objetos_echos/Alumno.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Objetos_echos;
+
+/**
+ *
+ * @author pomo6989
+ */
+public class Alumno extends Persona{
+    private String ciclo;
+    private int curso;
+    
+    //constructor
+    public Alumno(String ciclo, int curso, String DNI, String nombre) {
+        super(DNI, nombre); //llama al constructor padre con los parametros 
+                       //con los parametros que necesita el constructor padre
+        this.ciclo = ciclo;
+        this.curso = curso;
+    }
+
+    @Override
+    public void MostrarPersona() {
+        super.MostrarPersona();
+        System.out.println("ciclo: "+ciclo+" en el curso "+ curso);
+    }
+
+    @Override
+    public String toString() {
+        return "Alumno "+nombre+" "+DNI+"-"+curso+"-"+ciclo; 
+    }
+     
+    
+}
---
+++ src/Objetos_echos/Profesor.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Objetos_echos;
+
+/**
+ *
+ * @author pomo6989
+ */
+public class Profesor extends Persona{
+    protected int cod_profesor;
+    protected String departamento;
+
+    //constructor
+    public Profesor(int cod_profesor, String departamento, String DNI, String nombre) {
+        super(DNI, nombre);
+        this.cod_profesor = cod_profesor;
+        this.departamento = "Informatica";
+    }
+    
+    //getter
+    public int getCod_profesor() {
+        return cod_profesor;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+    
+    //setter
+    public void setCod_profesor(int cod_profesor) {
+        this.cod_profesor = cod_profesor;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+    
+    //funciones
+    public String MostrarProfesor() {
+        return "Profesor{" + "cod_profesor=" + cod_profesor + ", departamento=" + departamento + '}';
+    }
+
+    @Override
+    public String toString() {
+        return "Profesor{"+"DNI= " +DNI+", Nombre= "+nombre+ ", cod_profesor=" + cod_profesor + ", departamento=" + departamento + '}'; 
+    }
+    
+    
+   
+    
+}
