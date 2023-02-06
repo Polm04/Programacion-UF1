840b2db13c90d05481364ca6f87caebf176e2425
Polm04 <pol.mr.2003@gmail.com>
2023-02-06 14:49:48 +0100

    herencia astros:planetas i satelites

---
+++ src/Objetos_echos/Astros.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Objetos_echos;
+
+/**
+ *
+ * @author pomo6989
+ */
+public class Astros {
+   protected int masa;
+   protected int gravedad;
+   protected int radio_ecuatorial;
+   
+   //constructor
+    public Astros(int masa, int gravedad, int temperatura_media) {
+        this.masa = masa;
+        this.gravedad = gravedad;
+        this.radio_ecuatorial = temperatura_media;
+    }
+   
+    //getter
+    public int getMasa() {
+        return masa;
+    }
+
+    public int getGravedad() {
+        return gravedad;
+    }
+
+    public int getTemperatura_media() {
+        return radio_ecuatorial;
+    }
+    
+    //setter
+    public void setMasa(int masa) {
+        this.masa = masa;
+    }
+
+    public void setGravedad(int gravedad) {
+        this.gravedad = gravedad;
+    }
+
+    public void setTemperatura_media(int temperatura_media) {
+        this.radio_ecuatorial = temperatura_media;
+    }
+    
+    //metodos
+    @Override
+    public String toString() {
+        return "Astros{" + "masa=" + masa + ", gravedad=" + gravedad + ", temperatura_media=" + radio_ecuatorial + '}';
+    }
+    
+}
---
+++ src/Objetos_echos/Planetas.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Objetos_echos;
+
+/**
+ *
+ * @author pomo6989
+ */
+public class Planetas extends Astros{
+    protected int distancia_al_sol;
+    protected boolean Tiene_satelites;
+    
+    //constructor
+
+    public Planetas(int distancia_al_sol, boolean Tiene_satelites, int masa, int gravedad, int temperatura_media) {
+        super(masa, gravedad, temperatura_media);
+        this.distancia_al_sol = distancia_al_sol;
+        this.Tiene_satelites = Tiene_satelites;
+    }    
+    
+    //getter
+    public int getDistancia_al_sol() {
+        return distancia_al_sol;
+    }
+
+    public boolean isTiene_satelites() {
+        return Tiene_satelites;
+    }
+    //setter
+    public void setDistancia_al_sol(int distancia_al_sol) {
+        this.distancia_al_sol = distancia_al_sol;
+    }
+
+    public void setTiene_satelites(boolean Tiene_satelites) {
+        this.Tiene_satelites = Tiene_satelites;
+    }
+    //metodos
+
+    @Override
+    public String toString() {
+        return super.toString()+" Planetas"+"{distancia_al_sol=" + distancia_al_sol + ", Tiene_satelites=" +Tiene_satelites+ '}';
+    }
+    
+    
+    
+}
